84eb341bc2e4e595f637fd759c9938cb75cb5acd
michalosman <michal.osman.997@gmail.com>
2021-06-22 16:31:22 +0200

    Add tests for receiveAttack & isGameOver

--- src/factories/tests/Gameboard.test.ts
+++ src/factories/tests/Gameboard.test.ts
@@ -60,4 +60,30 @@ describe('Gameboard', () => {
     expect(gameboard.isPlacementPossible(ship, 2, 4, true)).toBe(false)
     expect(gameboard.isPlacementPossible(ship, 2, 5, true)).toBe(true)
   })
+
+  test('receives attacks', () => {
+    gameboard.placeShip(ship, 1, 1, true)
+    gameboard.receiveAttack(1, 3)
+    expect(gameboard.board[1][3].hits.includes(2)).toBe(true)
+  })
+
+  test('keeps track of missed shots', () => {
+    gameboard.placeShip(ship, 1, 1, true)
+    gameboard.receiveAttack(1, 4)
+    expect(gameboard.missedShots[1][4]).toBe(true)
+  })
+
+  test('tells if game is over', () => {
+    gameboard.placeShip(ship, 1, 1, true)
+    expect(gameboard.isGameOver()).toBe(false)
+    gameboard.receiveAttack(1, 1)
+    gameboard.receiveAttack(1, 2)
+    gameboard.receiveAttack(1, 3)
+
+    gameboard.placeShip(new Ship(3), 5, 5, false)
+    gameboard.receiveAttack(5, 5)
+    gameboard.receiveAttack(6, 5)
+    gameboard.receiveAttack(7, 5)
+    expect(gameboard.isGameOver()).toBe(true)
+  })
 })
